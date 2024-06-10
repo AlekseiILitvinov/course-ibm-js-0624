f080adf4db3e4e0db629de85da6dc25b1a6f9c54
AlekseiILitvinov <litvinovai988@gmail.com>
2024-06-10 16:41:19 -0400

    function calls2

--- followersCount/followersCount.js
+++ followersCount/followersCount.js
@@ -6,6 +6,12 @@ function increaseCount() {
   checkCountValue();
 }
 
+function resetCount() {
+  count = 0;
+  displayCount();
+  alert("Your Followers count has been reset!");
+}
+
 function displayCount() {
     document.getElementById('countDisplay').innerHTML = count;
 }
